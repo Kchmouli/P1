00033d83718a484458ac30e74f25ee8c650f3b35
Kantipudi <S530469@nwmissouri.edu>
2019-01-21 13:08:01 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Declare and use a bool, char type.
+            // Implement at least one loop
+            // Implement at least one if or if-else branch with conditions
+            // Implement a switch statement
+            // Implement a simple function calculation (e.g. totalCost or hasEnough)
+            // Use Console.WriteLine and Console.ReadLine
+            // Use string interpolation ($"") and \n or \t
+
+            //Cricket app
+            //calculate batting average and strike rate of batsman
+            //Rate and set base price for player
+            //Take name, runs, notouts, innings, ballsfaced as input
+
+            var runs = 0;
+            var innings = 0;
+            var notOuts = 0;
+            var ballsFaced = 0;
+            var name = string.Empty;
+            var basePrice = 0.0m;
+            var strikeRate = 0.0;
+            var average = 0.0;
+
+            while(true){
+                
+            }
+        }
+    }
+}
